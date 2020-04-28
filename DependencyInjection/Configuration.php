243fcabe0f95b0e76362a6e96b127cdb6bfa4a82
cgaia <52681461+cgaia@users.noreply.github.com>
2020-04-28 13:47:14 -0500

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,11 +1,6 @@
 <?php
 /*
  * This file is part of the StateMachine package.
- *
- * (c) Alexandre Bacco
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 namespace winzou\Bundle\StateMachineBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
